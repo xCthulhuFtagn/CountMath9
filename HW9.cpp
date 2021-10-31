086768b44dc138c3daa8e23dbfcfd2a4389e1ae0
Maxim <mkolodyazhny@gmail.com>
2021-10-31 18:33:48 +0300

    it just works

--- HW9.cpp
+++ HW9.cpp
@@ -5,19 +5,7 @@
 #include <valarray>
 using namespace std;
 
-void Print(double a, double b, double c, double d, double P, double Q, double i)
-{
-    cout << endl
-         << "step " << i + 1 << endl;
-    cout << "a = " << a << endl
-         << "b = " << b << endl
-         << "c = " << c << endl
-         << "d = " << d << endl;
-    cout << "Q[" << i + 1 << "] = " << Q << endl
-         << "P[" << i + 1 << "] = " << P << endl;
-}
-
-void Progonka(vector<valarray<double>> A)
+void Progonka(vector<vector<double>> A, double begin, double end, double h)
 {
     size_t size = A.size();
     vector<double> P(size + 1), Q(size + 1), y(size);
@@ -25,11 +13,9 @@ void Progonka(vector<valarray<double>> A)
     // начало
     P[0] = 0;
     Q[0] = 0;
-    cout << "P[0] = 0\nQ[0] = 0\n";
     double a = 0, b = A[0][0], c = A[0][1], d = A[0][size];
     P[1] = -c / (b + a * P[0]);
     Q[1] = (d - a * Q[0]) / (b + a * P[0]);
-    Print(a, b, c, d, P[1], Q[1], 0);
     //обход циклом P[2] -> P[size-1] и аналогично для Q
     for (size_t i = 1; i < size - 1; ++i)
     {
@@ -39,7 +25,6 @@ void Progonka(vector<valarray<double>> A)
         d = A[i][size];
         P[i + 1] = -c / (b + a * P[i]);
         Q[i + 1] = (d - a * Q[i]) / (b + a * P[i]);
-        Print(a, b, c, d, P[i + 1], Q[i + 1], i);
     }
     //конец матрицы
     a = A[size - 1][size - 2];
@@ -48,60 +33,62 @@ void Progonka(vector<valarray<double>> A)
     d = A[size - 1][size];
     P[size] = -c / (b + a * P[size - 1]);
     Q[size] = (d - a * Q[size - 1]) / (b + a * P[size - 1]);
-    Print(a, b, c, d, P[size], Q[size], size - 1);
     cout << endl
          << "results:" << endl;
-    //обход для получения иксов
     y[size - 1] = Q[size];
     for (int i = size - 2; i >= 0; --i)
         y[i] = Q[i + 1] + P[i + 1] * y[i + 1];
-    for (size_t x = 0; x <= 1; x+=0.1)
-        cout << "y(" << x << ") = " << y[x*10] << endl;
+    for (double i = 0; begin + i * h <= end; ++i){
+        cout << "y(" << begin + i*h << ") = " << y[i] << endl;
+    }
 }
 
 double zero(double x) { return 0; }
 
-void FiniteDifferenceMethod(double a, double b, unsigned n,
+void FiniteDifferenceMethod(double a, double b, double h,
     double (*K)(double), double (*L)(double), double (*M)(double), double (*F)(double),
     double R, double S, double T, double V, double W, double Z) {
-    double h = (b - a) / (n - 1);
+    size_t n = (b - a) / h + 1;
     vector<double> x;
     for (unsigned i = 0; i < n; ++i) {
         x.push_back(a + i * h);
     }
-    vector<valarray<double>> matrix;
-    matrix.resize(n+1);
-    for (auto& line : matrix) {
-        line.resize(n + 1);
-        line.apply(zero);
-    }
+    vector<vector<double>> matrix;
+    matrix.resize(n);
+    for (auto& line : matrix) line.assign(n+1, 0);
     matrix[0][0] = -R / h + S;
     matrix[0][1] = R / h;
     matrix[0][n] = T;
-    for (unsigned i = 1; i < n; ++i) {
-        double x_i = x[i];
-        matrix[i][i - 1] = K(x_i) / pow(h, 2) - L(x_i) / (2 * h);
-        matrix[i][i] = -2 * K(x_i) + M(x_i);
-        matrix[i][i + 1] = K(x_i) / pow(h, 2) + L(x_i) / (2 * h);
-        matrix[i][n] = F(x_i);
+    for (unsigned i = 1; i + 1 < n; ++i) {
+        matrix[i][i - 1] = K(x[i]) / pow(h, 2) - L(x[i]) / (2 * h);
+        matrix[i][i] = -2 * K(x[i]) / pow(h, 2) + M(x[i]);
+        matrix[i][i + 1] = K(x[i]) / pow(h, 2) + L(x[i]) / (2 * h);
+        matrix[i][n] = F(x[i]);
     }
-    matrix[n][n - 2] = V / h;
-    matrix[n][n-1] = -(V / h + W);
-    matrix[n][n] = -Z;
-    Progonka(matrix);
+    matrix[n-1][n - 2] = V / h;
+    matrix[n-1][n-1] = - V / h - W;
+    matrix[n-1][n] = -Z;
+    for(auto line : matrix){
+        for(auto el : line){
+            cout << el << " ";
+        }
+        cout << endl;
+    }
+    Progonka(matrix, a, b, h);
 }
 
-double K(double x) { return x*x - 1; }
-double L(double x) { return x-3; }
-double M(double x) { return -1; }
-double F(double x) { return 0; }
+double K(double x) { return 2*x*x; }
+double L(double x) { return x; }
+double M(double x) { return 1; }
+double F(double x) { return 2*sqrt(x); }
 
 int main()
 {
-    double a = 0, b = 1, step = 0.1, R = 1, S = 0, T = 0, V = 1, W = 1, Z = -0.75;
+    double a = 1, b = 10, step = 2.25, R = 0, S = 1, T = 2, V = 0, W = 1, Z = 2*sqrt(10);
     ;
-    FiniteDifferenceMethod(a, b, (b - a) / step,
+    FiniteDifferenceMethod(a, b, step,
         K, L, M, F,
-        R, S, T, V, W, Z);
+        R, S, T,
+        V, W, Z);
     
 }
